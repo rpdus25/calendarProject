47e8ec85f9fdff094e7a9752b801daf43dcfdb55
Kimgyeyeon <rpdus25@cubm.co.kr>
2019-01-23 17:04:22 +0900

    'login예제재구현연습중'

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
-import configureStore from './store/configureStore';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
+import { store } from './_helpers';
+import { App } from './App';
 
-ReactDOM.render((
-  // 모든 컴포넌트에서 react router가 동작하기 위해서는 최상위 컴포넌트를
-  <Provider store={configureStore()}>
-    {/*//BrwoserRouter로 감싸야함*/}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-),document.getElementById('root'));
+// setup fake backend
+import { configureFakeBackend } from './_helpers';
+configureFakeBackend();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
+);
