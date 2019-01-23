432cead2244e71ae05a74989ac43e35ee3629d3f
Kimgyeyeon <rpdus25@cubm.co.kr>
2019-01-23 17:38:54 +0900

    'navbar'

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import './Navbar.css';
 import {NavLink} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 //상단 메뉴를 가지고 있는 컴포넌트
 class Navbar extends Component {
   render() {
+    const { user, users } = this.props;
     return (
 		<nav>
 			<div className="nav-wrapper">
 				<a className="brand-logo">VALUESOFT</a>
 				<div className="right">
 					<ul>
+						<li> <h1>{user.firstName}{user.lastName}님</h1></li>
 						<li>
 							<NavLink to="/"><i className="material-icons">vpn_key</i></NavLink>
 						</li>
 						<li>
-							<a><i className="material-icons">lock_open</i></a>
+							<Link to="/login"><i className="material-icons">lock_open</i></Link>
 						</li>
 					</ul>
 				</div>
--- src/container/HomePage/HomePage.js
+++ src/container/HomePage/HomePage.js
@@ -14,25 +14,20 @@ class HomePage extends React.Component {
     render() {
         const { user, users } = this.props;
         return (
-
-            <div className="col-md-6 col-md-offset-3">
-              <Navbar/>
-                <h1>Hi {user.firstName}!</h1>
-                <h3>Users from secure api end point:</h3>
-                {users.loading && <em>Loading users...</em>}
-                {users.error && <span className="text-danger">ERROR: {users.error}</span>}
-                {users.items &&
-                    <ul>
-                        {users.items.map((user, index) =>
-                            <li key={user.id}>
-                                {user.firstName + ' ' + user.lastName}
-                            </li>
-                        )}
-                    </ul>
-                }
-                <p>
-                    <Link to="/login">Logout</Link>
-                </p>
+            <div className="">
+              <Navbar user={user} />
+                {/*<h3>Users from secure api end point:</h3>*/}
+                {/*{users.loading && <em>Loading users...</em>}*/}
+                {/*{users.error && <span className="text-danger">ERROR: {users.error}</span>}*/}
+                {/*{users.items &&*/}
+                    {/*<ul>*/}
+                        {/*{users.items.map((user, index) =>*/}
+                            {/*<li key={user.id}>*/}
+                                {/*{user.firstName + ' ' + user.lastName}*/}
+                            {/*</li>*/}
+                        {/*)}*/}
+                    {/*</ul>*/}
+                {/*}*/}
             </div>
         );
     }
